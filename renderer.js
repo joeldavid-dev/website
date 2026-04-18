59cd537295b7875ac96f9e2fef426619f7801515
Joel David <jdcruzzamora@gmail.com>
2026-04-17 21:57:10 -0600

    Sección de proyectos agregada

--- renderer.js
+++ renderer.js
@@ -13,12 +13,37 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
 
     // Fondo animado
-    document.body.addEventListener("pointermove", (e) => {
-        const { currentTarget: el, clientX: x, clientY: y } = e;
-        const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
-        el.style.setProperty('--posX', x - l - w / 2);
-        el.style.setProperty('--posY', y - t - h / 2);
-    })
+    let posX = 0;
+    let posY = 0;
+    let randomXValue = 0;
+    let randomYValue = 0;
+    // Dimenciones de la ventana
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    while (true) {
+        // Aplicar el valor aleatorio a las variables CSS
+        if (posX < randomXValue) {
+            posX += 1;
+        } else if (posX > randomXValue) {
+            posX -= 1;
+        } else {
+            randomXValue = Math.floor(Math.random() * windowWidth);
+        }
+
+        if (posY < randomYValue) {
+            posY += 1;
+        } else if (posY > randomYValue) {
+            posY -= 1;
+        } else {
+            randomYValue = Math.floor(Math.random() * windowHeight);
+        }
+
+        document.documentElement.style.setProperty('--posX', posX);
+        document.documentElement.style.setProperty('--posY', posY);
+        // Esperar un tiempo antes de generar nuevos valores
+        await new Promise(resolve => setTimeout(resolve, 5));
+    }
+
 
     // Cargar traducciones y mostrarlas en la interfaz estática
     async function loadTranslations(languageCode) {
